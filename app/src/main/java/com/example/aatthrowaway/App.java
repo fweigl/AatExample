cc5f28ab90505c998f727f33743bad5feee4bd3e
florian weigl <fweigl@florians-mbp-2.asv.local>
2017-07-24 16:15:03 +0200

    interstitials

--- app/src/main/java/com/example/aatthrowaway/App.java
+++ app/src/main/java/com/example/aatthrowaway/App.java
@@ -1,66 +1,19 @@
 package com.example.aatthrowaway;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.intentsoftware.addapptr.AATKit;
-import com.intentsoftware.addapptr.BannerPlacementLayout;
+import com.intentsoftware.addapptr.PlacementSize;
 
-public class App extends Application implements AATKit.Delegate {
+public class App extends Application {
 
-    public AATKit.Delegate delegate;
+    int placementId = -1;
 
     @Override
     public void onCreate() {
         super.onCreate();
-
-        AATKit.init(this, this);
+        AATKit.init(this, null);
+        placementId = AATKit.createPlacement("Fullscreen", PlacementSize.Fullscreen);
     }
 
-    @Override
-    public void aatkitHaveAd(final int i) {
-        Log.d("yyy", "aatkitHaveAd");
-    }
-
-    @Override
-    public void aatkitNoAd(final int i) {
-        Log.d("yyy", "aatkitNoAd");
-    }
-
-    @Override
-    public void aatkitPauseForAd(final int i) {
-
-    }
-
-    @Override
-    public void aatkitResumeAfterAd(final int i) {
-
-    }
-
-    @Override
-    public void aatkitShowingEmpty(final int i) {
-
-    }
-
-    @Override
-    public void aatkitUserEarnedIncentive(final int i) {
-
-    }
-
-    @Override
-    public void aatkitObtainedAdRules(final boolean b) {
-
-    }
-
-    @Override
-    public void aatkitUnknownBundleId() {
-
-    }
-
-    @Override
-    public void aatkitHaveAdForPlacementWithBannerView(
-            final int i, final BannerPlacementLayout bannerPlacementLayout) {
-
-        delegate.aatkitHaveAdForPlacementWithBannerView(i, bannerPlacementLayout);
-    }
 }
